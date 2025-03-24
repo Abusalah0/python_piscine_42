ca93d882dbe71cd03379c3cd6778215eb9e1f300
Abusalah0 <109g83@gmail.com>
2025-03-24 04:00:32 +0300

    upload

---
+++ module2/ex02/password.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+password = "Python is awesome"
+input = input()
+
+if input == password:
+    print("ACCESS GRANTED")
+else:
+    print("ACCESS DENIED")
---
+++ module2/ex03/mult.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+n1 = input()
+n2 = input()
+
+n1 = float(n1)
+n2 = float(n2)
+
+result = n1 * n2
+
+print(result)
+
+if (result > 0):
+    print("The result is positive.")
+else:
+    print("The result is negative.")
